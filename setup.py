c8bcf09f218cd78327e7662104914eab0d99ae46
Travus <realtravus@outlook.com>
2020-04-08 20:11:41 +0200

    Improved setup file.

--- setup.py
+++ setup.py
@@ -8,25 +8,26 @@ if __name__ == "__main__":
         """Clear the terminal."""
         os.system('cls' if os.name == 'nt' else 'clear')  # Clear function that works across platforms.
 
-    discord_token = None
-    while not discord_token:  # Ask for token repeatedly until received.
-        clr()
-        print("Setting up bot...")
+    discord_token = ""
+    clr()
+    print("Setting up bot...")
+    while len(discord_token) < 32:  # Ask for token repeatedly until received.
         print("Please enter your Discord bot token:")
         discord_token = input("> ").replace(" ", "").strip()
+        clr()
+        if len(discord_token) < 32:
+            print("Invalid token length.")
     clr()
-    print("Setting up bot...")
     print("Please enter a description of the bot for the about command. (Optional)")
-    print("This needs to be under 2000 characters, does not support tab and indents.\nType _prefix_ for the bot prefix:")
-    bot_description = input("> ")  # Ask for bot description.
+    print("This needs to be under 2000 characters, does not support indent and newlines.\nType _prefix_ for the bot prefix:\n")
+    bot_description = input()  # Ask for bot description.
     clr()
-    print("Setting up bot...")
     print("Please enter additional credits for the about command. (Optional)")
-    print("Credit for the base bot and the library are added automatically.")
-    print("Should look something like this:\n\nName:\n\tMaintaining\n\tHosting\n\nName:\n\tProfile Image\n")
+    print("Credits for the base bot and the library are added automatically.")
+    print("Should look something like this:\n\nPerson:\n\tMaintaining\n\tHosting\n\nPerson:\n\tProfile Image\n")
     print("Inline linking supported; [Text](URL)\n")
     print("This needs to be under 1000 characters, use \\n and \\t for newline and indent:")
-    bot_additional_credits = input("> ")  # Ask for additional credits.
+    bot_additional_credits = input()  # Ask for additional credits.
     db_con = sqlite3.connect("database.sqlite")  # Connect to database.
     db = db_con.cursor()
     db.execute("CREATE TABLE IF NOT EXISTS settings(flag TEXT PRIMARY KEY NOT NULL, value TEXT)")  # Create tables.
