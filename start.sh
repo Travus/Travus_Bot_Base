5ba79f15cd308fad800084fb0b166855426d91f9
Travus <realtravus@outlook.com>
2020-05-21 19:00:47 +0200

    Start script no longer gives errors when restarting container.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm -r /usr/src/app/temp_modules/__pycache__
-mv /usr/src/app/temp_modules/* /usr/src/app/modules/
-rmdir /usr/src/app/temp_modules
+mv /usr/src/app/temp_modules/* /usr/src/app/modules/ 2> /dev/null
+rm -rf /usr/src/app/temp_modules/__pycache__
+rm -rf /usr/src/app/temp_modules
 python /usr/src/app/main.py
